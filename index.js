6b51dba88de072f2f3a3616b3b7cb9a959845f5a
CiaranOtter <62837202+CiaranOtter@users.noreply.github.com>
2022-08-10 11:01:09 +0200

    creating an interface for saving a new checklist item

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 import {Checklist, checklist_item} from "./components/checklist.js"
-const homeDir = "http://127.0.0.1:5500/"
+const homeDir = "http://localhost:8888/Projects/web_diary/"
 const checklist = new Checklist()
 
 let activeDay = new Date(Date.now())
+
+document.getElementById("save_new_checklist_item").addEventListener("click", saveNewChecklistItem);
+
+function saveNewChecklistItem() {
+    console.log("the button has been pressed and will now attempt to createa and save a new hecklist item")
+
+    let name = document.getElementById("new_checklist_item_name").value;
+    let dueDate = document.getElementById("new_check_item_due_date").value;
+
+    if (name == "") {
+        console.log("the name of the checklist item can not have a null name");
+        return;
+    }
+
+    
+    let createdDate = new Date();
+    dueDate = new Date(dueDate)
+
+    
+    console.log(name, dueDate)
+    let jsonArray =
+        {
+            "name": name,
+            "due_date": dueDate,
+            "created_date": createdDate,
+            "completed": false,
+            "completed_date": null
+        };
+    checklist.addItem(jsonArray);
+
+}
 
 document.body.onload = () => {
     console.log("hello wolrd!")
